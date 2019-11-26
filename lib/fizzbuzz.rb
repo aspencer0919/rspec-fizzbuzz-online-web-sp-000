80597acac6647c0ad5e897f0b69b6c765792f974
Adam Spencer <aspencer0919@gmail.com>
2019-11-26 22:59:03 +0000

    Automatically backed up by Learn

---
+++ lib/fizzbuzz.rb
@@ -0,0 +1,20 @@
+# Don't forget! This file needs to be 'required' in its spec file
+# See README.md for instructions on how to do this
+fizzbuzz = input
+
+def(fizzbuzz)
+  if(fizzbuzz)
+      fizzbuzz % 3 == 0
+      puts "Fizz"
+    elsif
+      fizzbuzz % 5 == 0
+      puts "Buzz"
+    elseif
+      fizzbuzz % 3 == 0 && fizzbuzz % 5 == 0
+      puts "Fizzbuzz"
+    else
+      puts ""
+  end
+end
+
+puts display
