7946d525422c948a8569019839b080952af82d92
Adam Spencer <aspencer0919@gmail.com>
2019-11-27 01:15:12 +0000

    Automatically backed up by Learn

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,6 +1,8 @@
 #!/usr/lib/fizzbuzz.rb
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+input = 1
+
 if(input)
   input % 3 == 0
   puts "Fizz"
