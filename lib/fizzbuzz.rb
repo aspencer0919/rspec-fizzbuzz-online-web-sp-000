d8d110f257af4e9a13b764500e13894abe4dd015
Adam Spencer <aspencer0919@gmail.com>
2019-11-27 01:16:58 +0000

    Automatically backed up by Learn

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -2,15 +2,16 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 input = 1
+fizzbuzz = input
 
-if(input)
-  input % 3 == 0
+if(fizzbuzz)
+  fizzbuzz % 3 == 0
   puts "Fizz"
 elsif
-  input % 5 == 0
+  fizzbuzz % 5 == 0
   puts "Buzz"
 elsif
-  input % 3 == 0 && fizzbuzz % 5 == 0
+  fizzbuzz % 3 == 0 && fizzbuzz % 5 == 0
   puts "FizzBuzz"
 else
   puts ""
