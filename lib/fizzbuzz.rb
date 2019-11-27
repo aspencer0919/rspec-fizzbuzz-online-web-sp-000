2cc8b41f7cdc4ec3a9bb9cd33abab4fccc2715fc
Adam Spencer <aspencer0919@gmail.com>
2019-11-27 02:34:15 +0000

    Automatically backed up by Learn

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,17 +1,9 @@
 #!/usr/lib/fizzbuzz.rb
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-number = 1
 fizzbuzz = 1
 
-def number
-  number = ""
-end
-def input
-  input = number
-end
 def fizzbuzz
-  fizzbuzz = input
 end
 
 if fizzbuzz % 3 == 0 && fizzbuzz % 5 == 0
