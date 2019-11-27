47276113ae3fa268fce55926f9890581f585ac5d
Adam Spencer <aspencer0919@gmail.com>
2019-11-27 00:53:27 +0000

    Automatically backed up by Learn

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,10 +1,6 @@
 #!/usr/lib/fizzbuzz.rb
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def input
-  input = gets.chomp
-end
-
 if(input)
   input % 3 == 0
   puts "Fizz"
